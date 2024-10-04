62ae3e3f953ad38dbf6cf20054ba3350e0baaa62
Luthando Ntshanga <79196614+LuthandoNtshanga@users.noreply.github.com>
2024-10-04 10:28:37 +0200

    Add files via upload

---
+++ build/precache-manifest.bac933a8644920e252390aa1aafb0ac1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46500cdab78a6906d5562888f7388cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17d5ca2deff583cc677f",
+    "url": "/static/css/main.ded3d8b2.chunk.css"
+  },
+  {
+    "revision": "a802983af4a99bb19108",
+    "url": "/static/js/2.e6257e48.chunk.js"
+  },
+  {
+    "revision": "17d5ca2deff583cc677f",
+    "url": "/static/js/main.e7fb2fb9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
